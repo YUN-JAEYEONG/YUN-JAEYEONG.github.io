a455bc4c940e0fa17defabbf73375416482e3e01
윤재영 <22112106@yu.ac.kr>
2026-01-28 16:22:49 +0900

    marcov decision process

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1769520494',resources: [
+    cacheName: 'chirpy-1769584930',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
